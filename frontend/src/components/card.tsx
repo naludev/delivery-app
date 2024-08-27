6d1b206c734ebb8d678d101f766258a1bcd09f08
Nalú <nalum.dev@gmail.com>
2024-08-26 23:48:08 -0300

    delete unused import

--- frontend/src/components/card.tsx
+++ frontend/src/components/card.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Text from "@components/text";
 import Rating from "@components/rating";
-import Trago from '@assets/trago.jpg'
 
 interface CardProps {
   imageUrl?: string;
